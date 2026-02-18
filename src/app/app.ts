78d4b43fc8417e7263f4003bf7a64db71bb1a828
Amit260776 <amit.wolff@gmail.com>
2026-02-18 22:00:40 +0200

    Install and configure Material

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule, MatExpansionPanelHeader, MatExpansionPanel, MatExpansionPanelTitle } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatExpansionPanelHeader, MatExpansionPanel, MatExpansionPanelTitle],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
