b8b6b0bac69273d76d8f564eabac9ecdc0a6c563
Amit260776 <amit.wolff@gmail.com>
2026-02-20 21:15:23 +0200

    End Structure All Files And Forlders

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { ShellComponent } from './core/layout/shell.component/shell.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+        {
+            path: 'dashboard',
+            loadChildren: () => 
+                import('./features/dashboard/dashboard.routes')
+                    .then(m => m.DASHBOARD_ROUTES)
+        },
+        {
+            path: 'reports',
+            loadChildren: () => 
+                import('./features/reports/reports.routes')
+                    .then(m => m.REPORTS_ROUTES)
+        }        
+    ],
+  },
+];
---
+++ src/app/shared/directives/has-role.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HasRole } from './has-role.directive';
+
+describe('HasRole', () => {
+  it('should create an instance', () => {
+    const directive = new HasRole();
+    expect(directive).toBeTruthy();
+  });
+});
